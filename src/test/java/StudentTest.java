5bc4c909c8141f26a94cb542743b41474b2a1d44
chandan.singh@scaler.com <chandan.singh@scaler.com>
2023-04-07 17:24:07 +0530

    test: Updated test cases

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -20,93 +20,109 @@ class StudentTest {
     }
 
     @Test
-    void ageExists(){
+    void ageExists() throws Exception {
         try {
             Field ageField = student.getClass().getDeclaredField("age");
             assertEquals(ageField.getType().toString(), "int");
-        } catch (NoSuchFieldException e) {
+        } catch (Exception e) {
             fail("age data member of type int not found");
         }
     }
 
     @Test
-    void nameExists(){
+    void nameExists() throws Exception {
         try {
             Field nameField = student.getClass().getDeclaredField("name");
             assertEquals(nameField.getType().toString(), "class java.lang.String");
-        } catch (NoSuchFieldException e) {
+        } catch (Exception e) {
             fail("name data member of type String not found");
         }
     }
 
     @Test
-    void displayMethodExists(){
+    void displayMethodExists() throws Exception {
         try {
             Method displayMethod = student.getClass().getDeclaredMethod("display");
-        } catch (NoSuchMethodException e) {
+        } catch (Exception e) {
             fail("display method not found");
         }
     }
 
     @Test
-    void displayMethodSignatureCheck() throws NoSuchMethodException {
-        Method displayMethod = student.getClass().getDeclaredMethod("display");
-        assertEquals(displayMethod.toString(), "void Student.display()");
+    void displayMethodSignatureCheck() throws Exception {
+        try {
+            Method displayMethod = student.getClass().getDeclaredMethod("display");
+            assertEquals(displayMethod.toString(), "void Student.display()");
+        } catch (Exception e) {
+             fail("displayMethod method not found");
+        }
     }
 
     @Test
-    void sayHelloMethodExists(){
+    void sayHelloMethodExists() throws Exception {
+        try {
+           Method sayHelloMethod = student.getClass().getDeclaredMethod("sayHello", String.class);
+        } catch (Exception e) {
+             fail("sayHello method not found");
+        }
+    }
+
+    @Test
+    void sayHelloMethodSignatureCheck() throws Exception {
         try {
             Method sayHelloMethod = student.getClass().getDeclaredMethod("sayHello", String.class);
-        } catch (NoSuchMethodException e) {
+            assertEquals(sayHelloMethod.toString(), "void Student.sayHello(java.lang.String)");
+        } catch (Exception e) {
             fail("sayHello method not found");
         }
     }
 
     @Test
-    void sayHelloMethodSignatureCheck() throws NoSuchMethodException {
-        Method sayHelloMethod = student.getClass().getDeclaredMethod("sayHello", String.class);
-        assertEquals(sayHelloMethod.toString(), "void Student.sayHello(java.lang.String)");
+    void displayCheck() throws Exception {
+        try {
+            // student.age = 10
+            Field ageField = student.getClass().getDeclaredField("age");
+            ageField.setAccessible(true);
+            ageField.set(student, 10);
+
+            // student.name = "Test"
+            Field nameField = student.getClass().getDeclaredField("name");
+            nameField.setAccessible(true);
+            nameField.set(student, "Test");
+
+            outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+
+            // student.display()
+            Method displayMethod = student.getClass().getDeclaredMethod("display");
+            displayMethod.invoke(student);
+
+            String expected = "My name is Test. I am 10 years old\n";
+            assertEquals(expected, outContent.toString());
+        } catch (Exception e) {
+            fail("Exception occurred: " + e.getMessage());
+        }
     }
 
     @Test
-    void displayCheck() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        // student.age = 10
-        Field ageField = student.getClass().getDeclaredField("age");
-        ageField.setAccessible(true);
-        ageField.set(student, 10);
+    void sayHelloCheck() throws Exception {
+        try {
+            // student.name = "Test"
+            Field nameField = student.getClass().getDeclaredField("name");
+            nameField.setAccessible(true);
+            nameField.set(student, "Test");
 
-        // student.name = "Test"
-        Field nameField = student.getClass().getDeclaredField("name");
-        nameField.setAccessible(true);
-        nameField.set(student, "Test");
+            outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
 
-        outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
+            // student.sayHello("Test2");
+            Method sayHelloMethod = student.getClass().getDeclaredMethod("sayHello", String.class);
+            sayHelloMethod.invoke(student, "Test2");
 
-        // student.display()
-        Method displayMethod = student.getClass().getDeclaredMethod("display");
-        displayMethod.invoke(student);
-
-        String expected = "My name is Test. I am 10 years old\n";
-        assertEquals(expected, outContent.toString());
-    }
-
-    @Test
-    void sayHelloCheck() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        // student.name = "Test"
-        Field nameField = student.getClass().getDeclaredField("name");
-        nameField.setAccessible(true);
-        nameField.set(student, "Test");
-
-        outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        // student.sayHello("Test2");
-        Method sayHelloMethod = student.getClass().getDeclaredMethod("sayHello", String.class);
-        sayHelloMethod.invoke(student, "Test2");
-
-        String expected = "Test says hello to Test2\n";
-        assertEquals(expected, outContent.toString());
+            String expected = "Test says hello to Test2\n";
+            assertEquals(expected, outContent.toString());
+        } catch (Exception e) {
+            fail("Exception thrown: " + e);
+        }
     }
 }
